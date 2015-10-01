cbc84ab1bb3c67e4dc89fcf2ffdd1012b03ff805
Samuel Heaney <sheaney@gmail.com>
2015-09-30 20:53:31 -0500

    Refactor DB Insertion code

--- src/main/scala/co/triptailor/setup/db/DBTableInsertion.scala
+++ src/main/scala/co/triptailor/setup/db/DBTableInsertion.scala
@@ -13,7 +13,7 @@ class DBTableInsertion(implicit val ec: ExecutionContext) {
 
   def addHostelDependencies(document: RatedDocument) =
     for {
-      locationId ← idempotentInsertLocation(document.info.city, document.info.country)
+      locationId ← triptailorDB.run(idempotentInsertLocationQuery(document.info.city, document.info.country))
       hostelId   ← insertHostelInfo(document.info.name, document.reviews.size, locationId)
       _          ← insertHostelDependencies(document, hostelId)
     } yield hostelId
@@ -23,38 +23,32 @@ class DBTableInsertion(implicit val ec: ExecutionContext) {
 
   private def insertHostelDependencies(document: RatedDocument, hostelId: Int): Future[Int] =
     for {
-      serviceIds ← Future.sequence(document.info.services.map(idempotentInsertService))
-      _          ← insertHostelServices(hostelId, serviceIds)
-      reviewIds  ← Future.sequence(document.reviews.map(_.text).map(text => triptailorDB.run(insertReviewQuery(hostelId, text))))
-      _          ← insertAttributeReviews(hostelId, document.reviews, reviewIds)
-      _          ← insertHostelAttributes(hostelId, document.metrics)
+      serviceIds ← triptailorDB.run(DBIO.sequence(document.info.services.map(idempotentInsertServiceQuery)))
+      _          ← triptailorDB.run(DBIO.sequence(insertHostelServicesQueries(hostelId, serviceIds)))
+      reviewIds  ← triptailorDB.run(DBIO.sequence(document.reviews.map(review => insertReviewQuery(hostelId, review.text))))
+      _          ← triptailorDB.run(DBIO.sequence(insertAttributeReviewsQueries(hostelId, document.reviews, reviewIds)))
+      _          ← triptailorDB.run(DBIO.sequence(insertHostelAttributesQueries(hostelId, document.metrics)))
     } yield hostelId
 
-  private def insertAttributeReviews(hostelId: Int, reviews: Seq[RatedReview], reviewIds: Seq[Int]) =
-    Future.sequence {
-      reviews zip reviewIds flatMap { case (review, reviewId) =>
-        val tokens = review.tokens
-        tokens map { token =>
-          idempotentInsertAttribute(token.attribute) map { aid =>
-            val tokenPositions = token.positions.mkString(",")
-            triptailorDB.run(insertAttributeReviewQuery(AttributeReviewRow(aid, reviewId, tokenPositions)))
-          }
-        }
-      }
+  private def insertAttributeReviewsQueries(hostelId: Int, reviews: Seq[RatedReview], reviewIds: Seq[Int]) =
+    for {
+      (review, reviewId) ← reviews zip reviewIds
+      token              ← review.tokens
+    } yield idempotentInsertAttributeQuery(token.attribute) flatMap { aid =>
+      val tokenPositions = token.positions.mkString(",")
+      insertAttributeReviewQuery(AttributeReviewRow(aid, reviewId, tokenPositions))
     }
 
-  private def insertHostelAttributes(hostelId: Int, metrics: Map[String,RatingMetrics]) =
-    Future.sequence {
-      metrics.keySet map { attribute =>
-        idempotentInsertAttribute(attribute) map { aid =>
-          val m = metrics(attribute)
-          triptailorDB.run(insertHostelAttributeQuery(HostelAttributeRow(hostelId, aid, m.freq, m.cfreq, m.sentiment)))
-        }
-      }
+  private def insertHostelAttributesQueries(hostelId: Int, metrics: Map[String, RatingMetrics]) =
+    for {
+      attribute ← metrics.keys
+    } yield idempotentInsertAttributeQuery(attribute) map { aid =>
+      val m = metrics(attribute)
+      insertHostelAttributeQuery(HostelAttributeRow(hostelId, aid, m.freq, m.cfreq, m.sentiment))
     }
 
-  private def insertHostelServices(hostelId: Int, serviceIds: Seq[Int]) =
-    triptailorDB.run(DBIO.seq(serviceIds.map(sid => insertHostelServiceQuery(HostelServiceRow(hostelId, sid))): _*))
+  private def insertHostelServicesQueries(hostelId: Int, serviceIds: Seq[Int]) =
+    serviceIds.map(sid => insertHostelServiceQuery(HostelServiceRow(hostelId, sid)))
 
   private def insertHostelInfoQuery(name: String, noReviews: Int, locationId: Int) =
     Hostel.map(h => (h.name, h.noReviews, h.locationId)) returning Hostel.map(_.id) += (name, noReviews, locationId)
@@ -71,30 +65,30 @@ class DBTableInsertion(implicit val ec: ExecutionContext) {
   private def insertAttributeReviewQuery(row: AttributeReviewRow) =
     AttributeReview += row
 
-  private def idempotentInsertLocation(city: String, country: String): Future[Int] =
-    triptailorDB.run(Location.filter(l => l.city === city && l.country === country).map(_.id).result.headOption) flatMap { locationIdOpt =>
-      locationIdOpt.fold {
-        triptailorDB.run(Location.map(l => (l.city, l.country)) returning Location.map(_.id) += (city, country))
+  private def idempotentInsertLocationQuery(city: String, country: String) =
+    Location.filter(l => l.city === city && l.country === country).map(_.id).result.headOption flatMap { locationIdOpt =>
+      locationIdOpt.fold[DBIOAction[Int,NoStream,Effect.All]] {
+        Location.map(l => (l.city, l.country)) returning Location.map(_.id) += (city, country)
       } { locationId =>
-        Future.successful(locationId)
+        DBIO.successful(locationId)
       }
     }
 
-  private def idempotentInsertAttribute(name: String): Future[Int] =
-    triptailorDB.run(Attribute.filter(_.name === name).map(_.id).result.headOption) flatMap { attributeIdOpt =>
-      attributeIdOpt.fold {
-        triptailorDB.run(Attribute.map(_.name) returning Attribute.map(_.id) += name)
+  private def idempotentInsertAttributeQuery(name: String) =
+    Attribute.filter(_.name === name).map(_.id).result.headOption flatMap { attributeIdOpt =>
+      attributeIdOpt.fold[DBIOAction[Int,NoStream,Effect.All]] {
+        Attribute.map(_.name) returning Attribute.map(_.id) += name
       } { attributeId =>
-        Future.successful(attributeId)
+        DBIO.successful(attributeId)
       }
     }
 
-  private def idempotentInsertService(name: String): Future[Int] =
-    triptailorDB.run(Service.filter(_.name === name).map(_.id).result.headOption) flatMap { serviceIdOpt =>
-      serviceIdOpt.fold {
-        triptailorDB.run(Service.map(_.name) returning Service.map(_.id) += name)
+  private def idempotentInsertServiceQuery(name: String) =
+    Service.filter(_.name === name).map(_.id).result.headOption flatMap { serviceIdOpt =>
+      serviceIdOpt.fold[DBIOAction[Int,NoStream,Effect.All]] {
+        Service.map(_.name) returning Service.map(_.id) += name
       } { serviceId =>
-        Future.successful(serviceId)
+        DBIO.successful(serviceId)
       }
     }
 
