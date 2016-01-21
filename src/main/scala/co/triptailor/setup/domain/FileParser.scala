8ec3f6512273980e605d06b5c077ff766bbfa39e
Samuel Heaney <sheaney@gmail.com>
2016-01-20 22:54:27 -0600

    FileParser uses regex

--- src/main/scala/co/triptailor/setup/domain/FileParser.scala
+++ src/main/scala/co/triptailor/setup/domain/FileParser.scala
@@ -3,17 +3,16 @@ package co.triptailor.setup.domain
 import java.io.File
 
 object FileParser {
-  lazy val data = new File("./data")
+  lazy val data          = new File("./data")
+  lazy val relevantFiles = """(info.txt)|(reviews.txt)""".r
 
   def documentEntries =
     for {
       country  ← data.listFiles
       city     ← country.listFiles
       hostel   ← city.listFiles
-      txtFiles = hostel.listFiles.filter{ hostel => 
-        hostel.getName.equals("info.txt") || hostel.getName.equals("reviews.txt")
-      }
-      (generalFiles, reviewFiles) = txtFiles.partition(f => f.getName equals "info.txt")
+      files    = hostel.listFiles.filter(f => relevantFiles.pattern.matcher(f.getName).matches)
+      (generalFiles, reviewFiles) = files.partition(f => f.getName equals "info.txt")
       (generalFile, reviewFile) ← generalFiles zip reviewFiles
     } yield DocumentEntry(city.getName, country.getName, generalFile, reviewFile)
 
